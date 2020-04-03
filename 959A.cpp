140a4b0259ffb39ed4d0d41f9568da9c5652ad5f
Rik van der Kooij <rikvanderkooij@gmail.com>
2020-04-03 18:12:17 +0200

    Add solution for problem 959A

---
+++ 959A.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll n;
+    cin >> n;
+    if (n % 2) cout << "Ehab";
+    else cout << "Mahmoud";
+}
