648c9d644d8a09bb17456ccca238064b9b1cc231
Rik van der Kooij <rikvanderkooij@gmail.com>
2020-04-06 23:37:08 +0200

    Add solution for problem 1220A

---
+++ 1220A.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    int n;
+    string s;
+    cin >> n;
+    cin >> s;
+    for(auto i = count(begin(s), end(s), 'n'); i; --i) cout << "1 ";
+    for(auto i = count(begin(s), end(s), 'z'); i; --i) cout << "0 ";
+
+    for
+}
