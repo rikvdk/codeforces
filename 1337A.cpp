84a166974b0b59f1579d07bd6ff275171c7773a1
Rik van der Kooij <rikvanderkooij@gmail.com>
2020-04-17 23:35:47 +0200

    Add solution for problem 1337A

---
+++ 1337A.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    int t;
+    ll a, b, c, d;
+    cin >> t;
+    for(int i = 0; i < t; ++i) {
+        cin >> a >> b >> c >> d;
+        cout << b << ' ' << c << ' ' << c << '\n';
+    }
+}
