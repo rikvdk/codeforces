f98f6f6d87d09b48395fc47eee05aaf81dcd0de1
Rik van der Kooij <rikvanderkooij@gmail.com>
2020-04-25 12:59:48 +0200

    Add solution for problem 859B

---
+++ 859B.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    double n;
+    cin >> n;
+    cout << 2 * ceil(2*sqrt(n));
+}
