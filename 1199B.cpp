f5d5a892adeb66dd51ee2213a09f88a60f524fae
Rik van der Kooij <rikvanderkooij@gmail.com>
2020-04-25 12:58:41 +0200

    Add solution for problem 1199B

---
+++ 1199B.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    double H, L;
+    cin >> H >> L;
+
+    ZZ
+}
